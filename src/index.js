400223a9a528eaa610e78c9fa034b9990b3bdbb9
PaGenaro <80373174+PaGenaro@users.noreply.github.com>
2022-07-26 20:46:36 -0300

    "codigos"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default class App extends Component{
+  
+  state ={
+    num:0,
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+add = ()=>{
+  this.setState({num: this.state.num +1})
+}
+
+remove = ()=>{
+  this.setState({num: this.state.num -1})
+}
+  render(){
+    return(
+      <>
+        <h1>{this.state.num}</h1>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.remove}>-</button>
+      </>
+    )
+  }
+
+}
